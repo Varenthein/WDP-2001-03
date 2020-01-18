b4168ed97a443dfb463d045de66d1e38734826a4
Marysia Pabich <marysia.pabich@gmail.com>
2020-01-18 17:33:20 +0100

    Prepare basic component for product to be compared

---
+++ src/components/common/ComparedProduct/ComparedProduct.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './ComparedProduct.module.scss';
+
+const ComparedProduct = () => <div className={styles.component}>{/* <img /> */}</div>;
+
+ComparedProduct.propTypes = {
+  // comparison: PropTypes.array,
+};
+
+export default ComparedProduct;
