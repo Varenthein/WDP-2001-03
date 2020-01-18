a96db37e8774e4b904a69d5bce6b5abb91684bca
Marysia Pabich <marysia.pabich@gmail.com>
2020-01-18 19:10:44 +0100

    Add close button on ComparedProduct hover

--- src/components/common/ComparedProduct/ComparedProduct.js
+++ src/components/common/ComparedProduct/ComparedProduct.js
@@ -2,9 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './ComparedProduct.module.scss';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import Button from '../Button/Button';
+
 const ComparedProduct = ({ image }) => (
   <div className={styles.component}>
     <img src={image} alt='comparison-img' />
+    <div className={styles.imageHover}>
+      <Button>
+        <FontAwesomeIcon icon={faTimes}></FontAwesomeIcon>
+      </Button>
+    </div>
   </div>
 );
 
