be8b495daa64393f36b9b8e1e8a110b0f162c30d
Marysia Pabich <marysia.pabich@gmail.com>
2020-01-18 17:35:42 +0100

    Display products to comparison on sticky bar

--- src/components/features/StickyBar/StickyBar.js
+++ src/components/features/StickyBar/StickyBar.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './StickyBar.module.scss';
 
 import Button from '../../common/Button/Button';
+import ComparedProduct from '../../common/ComparedProduct/ComparedProduct';
 
 const StickyBar = ({ comparison }) =>
   comparison.length > 0 ? (
-    <div>
+    <div className={styles.component}>
+      {comparison.map(productData => (
+        <ComparedProduct key={productData.id} {...productData} />
+      ))}
       <Button>Compare</Button>
     </div>
   ) : (
