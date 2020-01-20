6d63458f7bec328e873593013309950ac363cea9
Magda Kwiatkowska-Witoń <magda-kwiatkowska@wp.pl>
2020-01-20 21:41:00 +0100

    Add general structure of Gallery component

--- src/components/features/Gallery/Gallery.js
+++ src/components/features/Gallery/Gallery.js
@@ -2,7 +2,25 @@ import React from 'react';
 import styles from './Gallery.module.scss';
 
 const Gallery = () => {
-  return <div className={styles.root}></div>;
+  return (
+    <div className={styles.root}>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-6'>
+            <div className={styles.heading}>
+              <h3>Furniture gallery</h3>
+            </div>
+            <div className={styles.menu}>
+              <a>category</a>
+            </div>
+            <div className={styles.product}>product</div>
+            <div className={styles.slider}>slider</div>
+          </div>
+          <div className='col-6'>picture</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Gallery;
