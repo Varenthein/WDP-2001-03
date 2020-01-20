ac3ad6f2a51a0d0c0e1a5ec1c16128e098890d59
Bartosz Iwiński <01bartosz0001@gmail.com>
2020-01-20 19:36:14 +0100

    Fix responsive in menuBar

--- src/components/features/ProductSearch/ProductSearch.js
+++ src/components/features/ProductSearch/ProductSearch.js
@@ -6,23 +6,30 @@ import { faListUl, faSearch, faCaretDown } from '@fortawesome/free-solid-svg-ico
 
 import styles from './ProductSearch.module.scss';
 
-const ProductSearch = () => (
-  <form action='' className={styles.root}>
-    <div className={styles.category}>
-      <FontAwesomeIcon className={styles.icon} icon={faListUl} />
-      <select name='' id=''>
-        <option value=''>Select a category</option>
-      </select>
-      <FontAwesomeIcon className={styles.icon} icon={faCaretDown} />
+const ProductSearch = () => {
+  const mql = window.matchMedia('(max-width: 767px)');
+  let mobile = false;
+  if (mql.matches) mobile = true;
+  return (
+    <div className={mobile ? styles.wrapMd : ''}>
+      <form action='' className={styles.root + ' ' + (mobile ? styles.mobile : '')}>
+        <div className={styles.category}>
+          <FontAwesomeIcon className={styles.icon} icon={faListUl} />
+          <select name='' id=''>
+            <option value=''>Select a category</option>
+          </select>
+          <FontAwesomeIcon className={styles.icon} icon={faCaretDown} />
+        </div>
+        <div className={styles.searchField}>
+          <input placeholder='Search products...' type='text' />
+          <button>
+            <FontAwesomeIcon className={styles.icon} icon={faSearch} />
+          </button>
+        </div>
+      </form>
     </div>
-    <div className={styles.searchField}>
-      <input placeholder='Search products...' type='text' />
-      <button>
-        <FontAwesomeIcon className={styles.icon} icon={faSearch} />
-      </button>
-    </div>
-  </form>
-);
+  );
+};
 
 ProductSearch.propTypes = {
   children: PropTypes.node,
--- src/components/features/ProductSearch/ProductSearch.test.js
+++ src/components/features/ProductSearch/ProductSearch.test.js
@@ -8,3 +8,17 @@ describe('Component ProductSearch', () => {
     expect(component).toBeTruthy();
   });
 });
+
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation(query => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // deprecated
+    removeListener: jest.fn(), // deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
