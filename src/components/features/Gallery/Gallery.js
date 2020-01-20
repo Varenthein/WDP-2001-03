6a0bb4456fc72f5092bd9f0f7e29ca2773799ec8
Magda Kwiatkowska-Witoń <magda-kwiatkowska@wp.pl>
2020-01-20 23:01:24 +0100

    Create GalleryContainer and add product's picture to the component

--- src/components/features/Gallery/Gallery.js
+++ src/components/features/Gallery/Gallery.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './Gallery.module.scss';
 
-const Gallery = () => {
+const Gallery = ({ products }) => {
   return (
     <div className={styles.root}>
       <div className='container'>
@@ -26,7 +27,9 @@ const Gallery = () => {
                 </li>
               </ul>
             </div>
-            <div className={styles.product}>product</div>
+            <div className={styles.product}>
+              <img src={products[0].image} alt='product 1' />
+            </div>
             <div className={styles.slider}>slider</div>
           </div>
           <div className='col-6'>picture</div>
@@ -34,6 +37,22 @@ const Gallery = () => {
       </div>
     </div>
   );
+};
+
+Gallery.propTypes = {
+  products: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string,
+      name: PropTypes.string,
+      category: PropTypes.string,
+      price: PropTypes.number,
+      stars: PropTypes.number,
+      promo: PropTypes.string,
+      newFurniture: PropTypes.bool,
+      favorite: PropTypes.bool,
+      image: PropTypes.string,
+    })
+  ),
 };
 
 export default Gallery;
--- src/components/features/Gallery/Gallery.test.js
+++ src/components/features/Gallery/Gallery.test.js
@@ -2,9 +2,24 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Gallery from './Gallery';
 
+const mockProps = [
+  {
+    id: 1,
+    name: 'bed',
+    category: 'bed',
+    price: 20,
+    oldPrice: 30,
+    stars: 2,
+    promo: 'sale',
+    newFurniture: true,
+    favorite: false,
+    image: 'image.src',
+  },
+];
+
 describe('Component Gallery', () => {
   it('should render without crashing', () => {
-    const component = shallow(<Gallery />);
+    const component = shallow(<Gallery {...mockProps}/>);
     expect(component).toBeTruthy();
   });
 });
