59c538ff196e6ea4fff8e3620ba73cb3a1265a7f
Marysia Pabich <marysia.pabich@gmail.com>
2020-01-23 20:57:52 +0100

    Define checkWidth function that returns mode according to viewport width

--- src/components/layout/MainLayout/MainLayout.js
+++ src/components/layout/MainLayout/MainLayout.js
@@ -4,13 +4,31 @@ import PropTypes from 'prop-types';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 
-const MainLayout = ({ children }) => (
-  <div>
-    <Header />
-    {children}
-    <Footer />
-  </div>
-);
+const checkWidth = () => {
+  const mobile = '(max-width: 576px)';
+  const tablet = '(max-width: 968px)';
+
+  if (window.matchMedia(`${mobile}`).matches) {
+    console.log('window match media for mobile');
+    return 'mobile';
+  } else if (window.matchMedia(`${tablet}`).matches) {
+    console.log('window match media for tablet');
+    return 'tablet';
+  } else {
+    console.log('window match media for desktop');
+    return 'desktop';
+  }
+};
+
+const MainLayout = ({ children }) => {
+  return (
+    <div>
+      <Header />
+      {children}
+      <Footer />
+    </div>
+  );
+};
 
 MainLayout.propTypes = {
   children: PropTypes.node,
