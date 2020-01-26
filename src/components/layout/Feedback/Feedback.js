4a1d58abbbf2bd7a7cfd109b2a66fc7a35a62060
Marysia Pabich <marysia.pabich@gmail.com>
2020-01-26 23:24:37 +0100

    Add quote icon under Feedback header

--- src/components/layout/Feedback/Feedback.js
+++ src/components/layout/Feedback/Feedback.js
@@ -1,6 +1,9 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faQuoteRight } from '@fortawesome/free-solid-svg-icons';
+
 import styles from './Feedback.module.scss';
 
 const Feedback = props => (
@@ -12,6 +15,9 @@ const Feedback = props => (
             <h3>Client Feedback</h3>
           </div>
         </div>
+      </div>
+      <div className='col-12'>
+        <FontAwesomeIcon icon={faQuoteRight} />
       </div>
     </div>
   </div>
