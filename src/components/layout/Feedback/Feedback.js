fec22da15405be67a7c15f8912718a9e846e2cf5
Marysia Pabich <marysia.pabich@gmail.com>
2020-01-26 17:51:41 +0100

    Create basics of Feedback component

---
+++ src/components/layout/Feedback/Feedback.js
@@ -0,0 +1,10 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+import styles from './Feedback.module.scss';
+
+const Feedback = props => <div className={styles.root}></div>;
+
+// Feedback.propTypes = {};
+
+export default Feedback;
