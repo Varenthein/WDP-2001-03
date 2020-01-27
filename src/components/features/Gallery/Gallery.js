f7648d61424b95f1a3ff88570435e60bc3a51fec
Magda Kwiatkowska-Witoń <magda-kwiatkowska@wp.pl>
2020-01-27 10:16:25 +0100

    Show only active photo in Gallery component

--- src/components/features/Gallery/Gallery.js
+++ src/components/features/Gallery/Gallery.js
@@ -4,13 +4,49 @@ import FadeIn from 'react-fade-in';
 import GalleryBox from '../../common/GalleryBox/GalleryBoxContainer';
 import styles from './Gallery.module.scss';
 import Button from '../../common/Button/Button';
+import ReactTooltip from 'react-tooltip';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faStar,
+  faExchangeAlt,
+  faShoppingBasket,
+  faEye,
+} from '@fortawesome/free-solid-svg-icons';
+import { faStar as farStar, faHeart } from '@fortawesome/free-regular-svg-icons';
 
 class Gallery extends React.Component {
   state = {
     activeTab: 'featured',
     filteredArr: this.props.products.filter(el => el.newFurniture === true),
     activePage: 0,
+    currentProduct: {
+      id: 'aenean-ru-bristique-1',
+      name: 'Aenean Ru Bristique 1',
+      price: 30,
+      oldprice: 35,
+      stars: 5,
+      promo: '',
+      newFurniture: true,
+      favorite: true,
+      image: 'https://cdn.pixabay.com/photo/2016/11/19/13/06/bed-1839183_1280.jpg',
+    },
   };
+
+  setNewCurrentProduct(el) {
+    this.setState({
+      currentProduct: {
+        id: el.id,
+        name: el.name,
+        price: el.price,
+        oldprice: el.oldprice,
+        stars: el.stars,
+        promo: el.promo,
+        newFurniture: el.newFurniture,
+        favorite: el.favorite,
+        image: el.image,
+      },
+    });
+  }
 
   moveRight() {
     const pagesCount = Math.ceil(this.state.filteredArr.length / 6);
@@ -56,13 +92,15 @@ class Gallery extends React.Component {
 
   render() {
     const { products } = this.props;
-    const { activeTab, filteredArr, activePage } = this.state;
+    const { activeTab, filteredArr, activePage, currentProduct } = this.state;
     const categories = [
       { id: 'featured', name: 'featured' },
       { id: 'topSeller', name: 'top seller' },
       { id: 'saleOff', name: 'sale off' },
       { id: 'topRated', name: 'top rated' },
     ];
+    let isActive = false;
+    console.log('isActive', isActive);
 
     return (
       <div className={styles.root}>
@@ -88,47 +126,94 @@ class Gallery extends React.Component {
                   ))}
                 </ul>
               </div>
-              <div className={styles.productSlider}>
-                <div className={styles.productWrapper}>
-                  {filteredArr.map(el => (
-                    <div key={el.id}>
-                      <FadeIn transitionDuration={2000}>
-                        <GalleryBox {...el} />
-                      </FadeIn>
+              <FadeIn transitionDuration={2000}>
+                <div className={styles.productSlider}>
+                  <div className={styles.product}>
+                    <img src={currentProduct.image} alt='product' />
+                    <div className={styles.buttons}>
+                      <ReactTooltip type='light' place='right' />
+                      <Button variant='gallery' data-tip='Add to favorites'>
+                        <FontAwesomeIcon icon={faHeart}>Favorite</FontAwesomeIcon>
+                      </Button>
+                      <Button variant='gallery' data-tip='Add to compare'>
+                        <FontAwesomeIcon icon={faExchangeAlt}>
+                          Add to compare
+                        </FontAwesomeIcon>
+                      </Button>
+                      <Button variant='gallery' data-tip='View details'>
+                        <FontAwesomeIcon icon={faEye}>View details</FontAwesomeIcon>
+                      </Button>
+                      <Button variant='gallery' data-tip='Add to basket'>
+                        <FontAwesomeIcon icon={faShoppingBasket}>
+                          Add to basket
+                        </FontAwesomeIcon>
+                      </Button>
                     </div>
-                  ))}
-                </div>
-                <div className={styles.slider}>
-                  <Button
-                    className={styles.prev}
-                    onClick={e => {
-                      e.preventDefault();
-                      this.moveLeft();
-                    }}
-                  >
-                    <p>{'<'}</p>
-                  </Button>
-                  <div className={styles.slides}>
-                    {filteredArr.slice(activePage * 6, (activePage + 1) * 6).map(el => (
-                      <img
-                        key={el.id}
-                        src={el.image}
-                        alt='pic'
-                        className={styles.slidePic}
-                      />
-                    ))}
+                    <div className={styles.content}>
+                      <div className={styles.triangleTopLeft} />
+                      <h5>{currentProduct.name}</h5>
+                      <div className={styles.stars}>
+                        {[1, 2, 3, 4, 5].map(i => (
+                          <a key={i} href='#'>
+                            {i <= currentProduct.stars ? (
+                              <FontAwesomeIcon icon={faStar}>{i} stars</FontAwesomeIcon>
+                            ) : (
+                              <FontAwesomeIcon icon={farStar}>
+                                {i} stars
+                              </FontAwesomeIcon>
+                            )}
+                          </a>
+                        ))}
+                      </div>
+                      <div className={styles.triangleBottomRight} />
+                      <div className={styles.price}>
+                        <h3>${currentProduct.price}</h3>
+                        <h5>${currentProduct.oldprice}</h5>
+                      </div>
+                    </div>
                   </div>
-                  <Button
-                    className={styles.next}
-                    onClick={e => {
-                      e.preventDefault();
-                      this.moveRight(filteredArr);
-                    }}
-                  >
-                    <p>{'>'}</p>
-                  </Button>
+                  <div className={styles.slider}>
+                    <Button
+                      className={styles.prev}
+                      onClick={e => {
+                        e.preventDefault();
+                        this.moveLeft();
+                      }}
+                    >
+                      <p>{'<'}</p>
+                    </Button>
+                    <div className={styles.slides}>
+                      {filteredArr
+                        .slice(activePage * 6, (activePage + 1) * 6)
+                        .map(el => (
+                          <img
+                            key={el.id}
+                            src={el.image}
+                            alt='pic'
+                            className={
+                              el.image === currentProduct.image
+                                ? styles.slidePic + ' ' + styles.active
+                                : styles.slidePic
+                            }
+                            onClick={e => {
+                              e.preventDefault();
+                              this.setNewCurrentProduct(el);
+                            }}
+                          />
+                        ))}
+                    </div>
+                    <Button
+                      className={styles.next}
+                      onClick={e => {
+                        e.preventDefault();
+                        this.moveRight();
+                      }}
+                    >
+                      <p>{'>'}</p>
+                    </Button>
+                  </div>
                 </div>
-              </div>
+              </FadeIn>
             </div>
             <div className={'col-6 ' + styles.picture}>
               <img src={products[2].image} alt='prod3' />
