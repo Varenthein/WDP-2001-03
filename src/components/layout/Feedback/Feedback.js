7857302455756c7c2a83115081ed42eed473676d
Marysia Pabich <marysia.pabich@gmail.com>
2020-01-27 18:54:58 +0100

    Map Comment components in Feedback section

--- src/components/layout/Feedback/Feedback.js
+++ src/components/layout/Feedback/Feedback.js
@@ -1,12 +1,14 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faQuoteRight } from '@fortawesome/free-solid-svg-icons';
 
+import Comment from '../../common/Comment/Comment';
+
 import styles from './Feedback.module.scss';
 
-const Feedback = props => (
+const Feedback = ({ feedback }) => (
   <div className={styles.root}>
     <div className='container'>
       <div className='row'>
@@ -19,10 +21,15 @@ const Feedback = props => (
       <div className='col-12'>
         <FontAwesomeIcon icon={faQuoteRight} />
       </div>
+      {feedback.map(comment => (
+        <Comment key={feedback.indexOf(comment)} {...comment} />
+      ))}
     </div>
   </div>
 );
 
-// Feedback.propTypes = {};
+Feedback.propTypes = {
+  feedback: PropTypes.array,
+};
 
 export default Feedback;
--- src/components/layout/Feedback/Feedback.test.js
+++ src/components/layout/Feedback/Feedback.test.js
@@ -2,8 +2,19 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Feedback from './Feedback';
 
+const mockProps = [
+  {
+    props1: 'test1',
+    props2: 'test2',
+  },
+  {
+    props1: 'test3',
+    props2: 'test4',
+  },
+];
+
 describe('Feedback', () => {
   it('renders without crashing', () => {
-    shallow(<Feedback />);
+    shallow(<Feedback feedback={mockProps} />);
   });
 });
