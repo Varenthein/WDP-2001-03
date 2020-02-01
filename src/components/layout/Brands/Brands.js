7e4699fb8223711ce453ce486c60c9fc26049ec9
Marcin Bajek <bajekmarcin@gmail.com>
2020-02-01 04:41:25 +0100

    Add requested changes

--- src/components/layout/Brands/Brands.js
+++ src/components/layout/Brands/Brands.js
@@ -4,37 +4,32 @@ import styles from './Brands.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons';
 
-class Brands extends React.Component {
-  static propTypes = {
-    image: PropTypes.node,
-    brands: PropTypes.array,
-  };
-  render() {
-    const { brands } = this.props;
-    return (
-      <div className={styles.root}>
-        <div className='container'>
-          <div className={styles.wrapper}>
-            <div className={styles.slide}>
-              <FontAwesomeIcon className={styles.icon} icon={faAngleLeft} />
-            </div>
-            <div className={styles.brandsWrapper}>
-              <div className={styles.swipable}>
-                {brands.map(brand => (
-                  <div className={styles.brand} key={brand.key} {...brand}>
-                    <img src={brand.image} alt='brand' />
-                  </div>
-                ))}
+const Brands = ({ brands }) => (
+  <div className={styles.root}>
+    <div className='container'>
+      <div className={styles.wrapper}>
+        <div className={styles.slide}>
+          <FontAwesomeIcon className={styles.icon} icon={faAngleLeft} />
+        </div>
+        <div className={styles.brandsWrapper}>
+          <div className={styles.swipable}>
+            {brands.map(brand => (
+              <div className={styles.brand} key={brand.key} {...brand}>
+                <img src={brand.image} alt='brand' />
               </div>
-            </div>
-            <div className={styles.slide}>
-              <FontAwesomeIcon className={styles.icon} icon={faAngleRight} />
-            </div>
+            ))}
           </div>
         </div>
+        <div className={styles.slide}>
+          <FontAwesomeIcon className={styles.icon} icon={faAngleRight} />
+        </div>
       </div>
-    );
-  }
-}
+    </div>
+  </div>
+);
 
+Brands.propTypes = {
+  image: PropTypes.node,
+  brands: PropTypes.array,
+};
 export default Brands;
