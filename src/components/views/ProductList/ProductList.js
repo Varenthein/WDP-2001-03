a5e71519a0d6724d0d752c47b0504798ffcf1288
Magda Kwiatkowska-Witoń <magda-kwiatkowska@wp.pl>
2020-02-02 21:37:27 +0100

    Add structure of banner to ProductList component

--- src/components/views/ProductList/ProductList.js
+++ src/components/views/ProductList/ProductList.js
@@ -2,7 +2,21 @@ import React from 'react';
 // import PropTypes from 'prop-types';
 import styles from './ProductList.module.scss';
 
-const ProductList = () => <div className={styles.root}>This is ProductList</div>;
+const ProductList = () => {
+  return (
+    <div className={styles.root}>
+      <div className={styles.banner}>
+        <h1 className={styles.heading}>
+          bedroom <span>furniture</span>
+        </h1>
+        <h2 className={styles.subheading}>
+          always <span>25%</span> off or more
+        </h2>
+      </div>
+      <div className={styles.breadcumb}>breadcumb</div>
+    </div>
+  );
+};
 
 // ProductList.propTypes = {};
 
