5d6c700343684b1a784dd15b05af80e112a11cdf
Marcin Bajek <bajekmarcin@gmail.com>
2020-02-05 05:10:06 +0100

    Add param category Id

--- src/components/views/ProductList/ProductList.js
+++ src/components/views/ProductList/ProductList.js
@@ -1,29 +1,33 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import styles from './ProductList.module.scss';
 import Banner from '../../layout/Banner/Banner';
 import Breadcrumb from '../../common/Breadcrumb/Breadcrumb';
 
-const breadcumbLinks = [
-  { id: 'home', label: 'Home' },
-  { id: 'furniture', label: 'Furniture' },
-];
+// const breadcumbLinks = [
+//   { id: 'home', label: 'Home' },
+//   { id: 'furniture', label: 'Furniture' },
+// ];
 
-const ProductList = () => {
+const ProductList = ({ match }) => {
   return (
     <div className={styles.root}>
       <div className='container'>
         <Banner />
         <Breadcrumb>
-          {breadcumbLinks.map(el => (
+          {/* {breadcumbLinks.map(el => (
             <a key={el.id}>{el.label}</a>
-          ))}
+          ))} */}
+          <a>Home</a>
+          <a key={match.params.categoryId}>{match.params.categoryId}</a>
         </Breadcrumb>
       </div>
     </div>
   );
 };
 
-// ProductList.propTypes = {};
+ProductList.propTypes = {
+  match: PropTypes.object,
+};
 
 export default ProductList;
