bc310dc2eeb709c0eb2f691a00639fd4e8ee9af4
Marysia Pabich <marysia.pabich@gmail.com>
2020-02-05 07:23:58 +0100

    Implement right & left actions as props for Swipe

--- src/components/features/Swipe/Swipe.js
+++ src/components/features/Swipe/Swipe.js
@@ -7,22 +7,32 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronRight, faChevronLeft } from '@fortawesome/free-solid-svg-icons';
 import Button from '../../common/Button/Button';
 
-const Swipe = ({ children }) => (
-  <div>
-    <Swipeable>
-      {children}
-      <Button>
-        <FontAwesomeIcon icon={faChevronLeft}></FontAwesomeIcon>
-      </Button>
-      <Button>
-        <FontAwesomeIcon icon={faChevronRight}></FontAwesomeIcon>
-      </Button>
-    </Swipeable>
-  </div>
+const Swipe = ({ children, leftAction, rightAction }) => (
+  <Swipeable>
+    {children}
+    <Button
+      onClick={e => {
+        e.preventDefault();
+        leftAction();
+      }}
+    >
+      <FontAwesomeIcon icon={faChevronLeft}></FontAwesomeIcon>
+    </Button>
+    <Button
+      onClick={e => {
+        e.preventDefault();
+        rightAction();
+      }}
+    >
+      <FontAwesomeIcon icon={faChevronRight}></FontAwesomeIcon>
+    </Button>
+  </Swipeable>
 );
 
 Swipe.propTypes = {
   children: PropTypes.node,
+  rightAction: PropTypes.func,
+  leftAction: PropTypes.func,
 };
 
 export default Swipe;
