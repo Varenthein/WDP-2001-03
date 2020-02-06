de0d0fa59560b96caacf25559beb45e5c45881b1
Marysia Pabich <marysia.pabich@gmail.com>
2020-02-06 07:52:39 +0100

    Add Swipe effect for mobile & tablets

--- src/components/features/Swipe/Swipe.js
+++ src/components/features/Swipe/Swipe.js
@@ -2,32 +2,35 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Swipe.module.scss';
 
+import { Swipeable } from 'react-swipeable';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronRight, faChevronLeft } from '@fortawesome/free-solid-svg-icons';
 import Button from '../../common/Button/Button';
 
 const Swipe = ({ children, leftAction, rightAction }) => (
-  <div className='row'>
-    {children}
-    <Button
-      className={styles.button}
-      onClick={e => {
-        e.preventDefault();
-        leftAction();
-      }}
-    >
-      <FontAwesomeIcon icon={faChevronLeft}></FontAwesomeIcon>
-    </Button>
-    <Button
-      className={styles.button}
-      onClick={e => {
-        e.preventDefault();
-        rightAction();
-      }}
-    >
-      <FontAwesomeIcon icon={faChevronRight}></FontAwesomeIcon>
-    </Button>
-  </div>
+  <Swipeable onSwipedLeft={() => rightAction()} onSwipedRight={() => leftAction()}>
+    <div className='row'>
+      {children}
+      <Button
+        className={styles.button}
+        onClick={e => {
+          e.preventDefault();
+          leftAction();
+        }}
+      >
+        <FontAwesomeIcon icon={faChevronLeft}></FontAwesomeIcon>
+      </Button>
+      <Button
+        className={styles.button}
+        onClick={e => {
+          e.preventDefault();
+          rightAction();
+        }}
+      >
+        <FontAwesomeIcon icon={faChevronRight}></FontAwesomeIcon>
+      </Button>
+    </div>
+  </Swipeable>
 );
 
 Swipe.propTypes = {
